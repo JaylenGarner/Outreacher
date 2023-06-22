712c543db90d72f0ec52b6ba625cced034b64575
Jaylen Garner <jaylen.garner@outlook.com>
2023-06-21 23:08:47 -0400

    Application Schema created. Added validations to User Schema.

---
+++ models/applicationModel.js
@@ -0,0 +1,52 @@
+import { Schema, Model, Models } from "mongoose";
+import { isUrl } from "validator";
+
+const applicationSchema = new Schema({
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    index: true,
+  },
+  company: {
+    type: String,
+    maxLength: [100, "Company name must not exceed 80 characters"],
+  },
+  position: {
+    type: String,
+    maxLength: [100, "Position title must not exceed 80 characters"],
+  },
+  posting: {
+    type: String,
+    maxLength: [2048, "Posting URL must not exceed 2048 characters"],
+    validate: [isUrl, "Invalid URL"],
+  },
+  salary: {
+    type: String,
+    maxLength: [50, "Salary range must not exceed 50 characters"],
+  },
+  location: {
+    type: String,
+    maxLength: [500, "Location must not exceed 500 characters"],
+  },
+  notes: {
+    type: String,
+    maxLength: [
+      2000,
+      "Notes for this application must not exceed 2000 characters",
+    ],
+  },
+  status: {
+    type: String,
+    enum: ["queue", "applied", "rejected", "interview", "hired"],
+    maxLength: [9, "Application status must not exceed 9 characters"],
+  },
+  createdAt: {
+    type: Date,
+    immutable: true,
+    default: () => Date.now(),
+  },
+  updatedAt: {
+    type: Date,
+    default: () => Date.now(),
+  },
+});
--- models/userModel.js
+++ models/userModel.js
@@ -1,36 +1,43 @@
 import { Schema, Model, Models } from "mongoose";
+import { isEmail } from "validator";
 
 const userSchema = new Schema({
   firstName: {
     type: String,
     required: [true, "First name is required"],
-    minLength: 2,
-    maxLength: 20,
+    maxLength: [30, "First name must not exceed 30 characters"],
   },
-  lastname: {
+  lastName: {
     type: String,
     required: [true, "Last name is required"],
-    minLength: 2,
-    maxLength: 20,
+    maxLength: [30, "Last name must not exceed 30 characters"],
   },
   email: {
     type: String,
     required: [true, "Email is required"],
-    minLength: 2,
-    maxLength: 50,
+    unique: [true, "Email is in use"],
+    validate: [isEmail, "Invalid email"],
+    maxLength: [50, "Email must not exceed 50 characters"],
   },
   username: {
     type: String,
     required: [true, "Username is required"],
-    minLength: 2,
-    maxLength: 20,
+    unique: [true, "Username is in use"],
+    minLength: [2, "Username must be at least 2 characters"],
+    maxLength: [20, "Username must not exceed 20 characters"],
   },
   password: {
     type: String,
     required: [true, "Password is required"],
-    minLength: 8,
-    maxLength: 20,
+    minLength: [8, "Password must be at least 8 characters"],
+    maxLength: [20, "Password must not exceed 20 characters"],
   },
+  applications: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Application",
+    },
+  ],
   createdAt: {
     type: Date,
     immutable: true,
@@ -42,4 +49,4 @@ const userSchema = new Schema({
   },
 });
 
-export const User = mongoose.model("User", userSchema);
+// export const User = mongoose.model("User", userSchema);
