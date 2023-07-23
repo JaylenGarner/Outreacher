30ad3daa48cfb0404b7bc2d1638bfb30c67a9f42
Jaylen Garner <jaylen.garner@outlook.com>
2023-07-23 18:14:19 -0400

    Logic logic migrated to postgres

--- src/app/api/login/route.js
+++ src/app/api/login/route.js
@@ -1,25 +1,36 @@
-import User from "../../../../models/User";
-import { dbConnect } from "../../../../lib/db";
+// import User from "../../../../models/User";
+// import { dbConnect } from "../../../../lib/db";
+import prisma from "../../../../lib/prisma";
 import * as bcrypt from "bcrypt";
 
 export const POST = async (req) => {
   try {
     const body = await req.json();
     const { email, password } = body;
-    await dbConnect();
+    // await dbConnect();
 
-    const user = await User.find({
-      email,
+    console.log("Start of login route");
+
+    const user = await prisma.user.findUnique({
+      where: {
+        email,
+      },
     });
 
-    if (user && (await bcrypt.compare(password, user[0].password))) {
-      const { password, ...userWithoutPassword } = user[0].toObject();
+    console.log("USER", user);
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const { password, ...userWithoutPassword } = user;
+
+      console.log("user without password", userWithoutPassword);
 
       return new Response(JSON.stringify(userWithoutPassword));
     } else {
+      console.log("HIT THE ELSE");
       return new Response(JSON.stringify(null));
     }
   } catch (error) {
+    console.log("ERROR");
     return new Response("The email or password is incorrect");
   }
 };
