54e7c7570475ec7929e8d716576ce636606465b9
Jaylen Garner <jaylen.garner@outlook.com>
2023-09-10 14:56:27 -0400

    Info Modal update

--- components/Info.jsx
+++ components/Info.jsx
@@ -5,13 +5,24 @@ const Info = () => {
   return (
     <div className="pt-4 pb-4 pr-10 pl-10 flex_center flex-col space-y-2">
       <h2 className="modal_header">Information</h2>
-      <p className="max-w-[550px] text-lg pb-4 text-center">
+      <p className="max-w-[550px] text-lg text-center">
         The Outreach Algorithm streamlines follow-up management by considering
         the outreach stage and date, resulting in a daily contact list. It
         maintains a consistent two-day interval for balanced engagement and
         accommodates weekends and US holidays by rescheduling follow-ups to the
         next available business day.
       </p>
+
+      <span className="text-lg text-center">
+        A full feature breakdown is available{" "}
+        <motion.a
+          whileHover={{ color: "#FF0066" }}
+          href="https://github.com/JaylenGarner/Outreacher/blob/main/README.md"
+          className="font-bold"
+        >
+          here
+        </motion.a>
+      </span>
 
       <span className="text-lg text-center">
         Your input is valuable! Share feedback or feature suggestions:
@@ -43,7 +54,7 @@ const Info = () => {
         <Link href="https://github.com/JaylenGarner" target="_blank">
           <motion.img
             src="/github-logo.png"
-            className="w-[76px]"
+            className="w-[76px] mb-1"
             whileHover={{ opacity: 0.6 }}
           ></motion.img>
         </Link>
